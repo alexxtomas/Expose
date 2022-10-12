702e58888b4ec85a1638e2ad2e6be30409363818
Alex Tomas <atomasllimos@gmail.com>
2022-10-13 00:17:21 +0200

    Add new content in Home Page

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -6,6 +6,17 @@ const Home = () => {
     <p>
     Expose allows you to post anything without any restrictions or censorship. It is the website of the people, where everyone can expose their ideas and create their content whatever the content is. Here you have no limit!
     </p>
+    <h3>Easily create a post, and get feedback from people. <a href="https://github.com/alexxtomas/Expose" target="__blank">This project is fully open source and you can participate in it!</a>
+    </h3>
+
+    <h2>ROADMAP:</h2>
+    <ul>
+      <li>To be able to respond to the comments of your post</li>
+      <li> Being able to add images to your post/li>
+      <li>Being able to filter the posts by topics that interest you.</li>
+      <li>Being able to react to emoticons to the</li>
+    </ul>
+    </h2>
   `
 }
 export default Home
